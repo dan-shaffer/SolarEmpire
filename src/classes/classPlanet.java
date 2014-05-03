a1d7de140a2ffdcfed27b3c7ffbe55f150b6d7cb
dan-shaffer <shaffer2112@gmail.com>
2014-05-03 06:38:17 -0400

    planet class

--- src/classes/classPlanet.java
+++ src/classes/classPlanet.java
@@ -1,15 +1,27 @@
+package classes;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package classes;
-
 /**
  *
  * @author Dan
  */
 public class classPlanet {
+    
+    public String Name;
+    public int PlanetID;
+    public int Metal;
+    public int Gas;
+    public boolean habitable;
+    public double TerraForm;
+    
+    classPlanet(int PlanetID) {
+        // OPEN DATABASE AND POPULATE FIELDS
+    }
+    
     
 }
